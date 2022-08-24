ea068355961e05dac1f772ba631dc3fe79c6cbaf
Edward Staples <1023mb@gmail.com>
2022-08-24 12:30:53 -0400

    Created account daos

---
+++ tenmo/server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -0,0 +1,39 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class JdbcAccountDao implements AccountDao{
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate){this.jdbcTemplate = jdbcTemplate;}
+
+
+    @Override
+    public BigDecimal getBalance(int id) {
+        String sql = "SELECT balance FROM account JOIN tenmo_user ON account.user_id = tenmo_user.user_id WHERE account.user_id = ?;";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql,id);
+        if(rowSet.next()){
+            Account account = mapRowToAccount(rowSet);
+            return account.getBalance();
+        }
+
+        return null;
+    }
+
+
+    private Account mapRowToAccount(SqlRowSet rowSet){
+        Account account = new Account();
+        account.setAccount_id(rowSet.getInt("account_id"));
+        account.setUser_id(rowSet.getInt("user_id"));
+        account.setBalance(rowSet.getBigDecimal("balance"));
+
+        return account;
+    }
+}
